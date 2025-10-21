c54dc8497fd5591ff6733925c7c58234a6f437a3
nelifzyh <nelifauziyah07@gmail.com>
2025-10-21 13:18:29 +0700

    model country

---
+++ app/Models/Country.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Country extends Model
+{
+    protected $table = 'country';
+    protected $primaryKey = 'companydesignid';
+    public $timestamps = false;
+
+    protected $fillable = [
+        'countryid',
+        'name',
+        'isocode',
+        'shortcode',
+        'sysrowid',
+        'createdby',
+        'createdon',
+        'updatedby',
+        'updatedon',
+    ];
+}
