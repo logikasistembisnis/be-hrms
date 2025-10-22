1c75926ead9f6a4ec018fd2f75884df9c567405c
nelifzyh <nelifauziyah07@gmail.com>
2025-10-22 16:49:55 +0700

    update cors

--- app/Http/Middleware/CorsMiddleware.php
+++ app/Http/Middleware/CorsMiddleware.php
@@ -6,25 +6,13 @@ use Closure;
 
 class CorsMiddleware
 {
-    /**
-     * Handle an incoming request.
-     */
     public function handle($request, Closure $next)
     {
         $response = $next($request);
 
-        $response->headers->set('Access-Control-Allow-Origin', 'http://192.168.61.199:3000');
+        $response->headers->set('Access-Control-Allow-Origin', '*');
         $response->headers->set('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-        $response->headers->set('Access-Control-Allow-Headers', 'Content-Type, Authorization, X-Requested-With');
-        $response->headers->set('Access-Control-Allow-Credentials', 'true');
-
-        if ($request->getMethod() === 'OPTIONS') {
-            return response('', 200)
-                ->header('Access-Control-Allow-Origin', 'http://192.168.61.199:3000')
-                ->header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS')
-                ->header('Access-Control-Allow-Headers', 'Content-Type, Authorization, X-Requested-With')
-                ->header('Access-Control-Allow-Credentials', 'true');
-        }
+        $response->headers->set('Access-Control-Allow-Headers', 'Content-Type, Authorization');
 
         return $response;
     }
